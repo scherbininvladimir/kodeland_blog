1162075c6b80a00bd0ec9e0801afde6858d7fa07
Vladimir <scherbinin.vladimir@gmail.com>
2020-08-30 00:31:40 +0300

    deploy

--- kodeland_blog/urls.py
+++ kodeland_blog/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
-    path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('blog/', include('blog.urls')),
+]
